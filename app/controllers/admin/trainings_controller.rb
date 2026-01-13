7996f745ee8043816bc838cd92b234fcfb69c650
TasukuIgo <igo@fundely.co.jp>
2026-01-13 10:38:05 +0900

    パスワード仮機能作成前Migrate

--- app/controllers/admin/trainings_controller.rb
+++ app/controllers/admin/trainings_controller.rb
@@ -1,20 +1,24 @@
 class Admin::TrainingsController < ApplicationController
+
+# 新規作成ページ用のnewアクション
   def new
     @training = Training.new
   end
 
+# 
   def create
     @training = Training.new(training_params)
     
     if @training.save
       redirect_to admin_training_path(@training), 
-      notice:"研修を作成しました。"
+      notice: "研修を作成しました。"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def index
+    @trainings = Training.order(created_at: :desc)
   end
 
   def show
@@ -22,6 +26,18 @@ class Admin::TrainingsController < ApplicationController
   end
 
   def edit
+    @training = Training.find(params[:id])
+  end
+
+  def update
+    @training = Training.find(params[:id])
+
+    if @training.update(training_params)
+      redirect_to admin_training_path(@training),
+      notice: "研修内容を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+ def dashboard_path_by_role
+    if current_user.admin?
+      admin_dashboard_path
+    else
+      dashboard_path
+    end
+  end
 end
