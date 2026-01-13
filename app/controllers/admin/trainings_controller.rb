45fd122326e7ca513ab7d977491133734a3f661d
TasukuIgo <igo@fundely.co.jp>
2026-01-13 10:03:55 +0900

    データが保存されないためdb:migrate前の保存。

--- app/controllers/admin/trainings_controller.rb
+++ app/controllers/admin/trainings_controller.rb
@@ -1,13 +1,32 @@
 class Admin::TrainingsController < ApplicationController
   def new
+    @training = Training.new
+  end
+
+  def create
+    @training = Training.new(training_params)
+    
+    if @training.save
+      redirect_to admin_training_path(@training), 
+      notice:"研修を作成しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
   end
 
   def show
+    @training = Training.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def training_params
+    params.require(:training).permit(:title, :description)
   end
 end
